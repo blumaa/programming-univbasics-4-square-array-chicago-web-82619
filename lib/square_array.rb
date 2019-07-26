47d34cf1238a2cc52471f35c3b835a8951be0f2b
Aaron Blum <blumaa@gmail.com>
2019-07-26 18:00:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
-require 'pry'
 
 def square_array(array)
   i = 0
+  new_array = array.new
   while i < array.length do
-    array[i] ** 2
+    new_array << array[i] ** 2
     i += 1
   end
+  new_array
 end
